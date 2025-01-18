6985c8f5d53366aedcdf79f32ad4d94dfd41b3cb
Sa.Amini <Sa.Amini@sadad.co.ir>
2025-01-18 15:53:14 +0330

    Add project files.

---
+++ visitor/FileUtility.cs
@@ -0,0 +1,104 @@
+﻿namespace visitor
+{
+
+    public interface IFileUtility
+    {
+        public long GetFileNumber(ICollection<FileInfo> files);
+        public long GetFileSize(ICollection<FileInfo> files);
+    }
+
+
+    public class AllFileUtility : IFileUtility
+    {
+        void Accept(IFileVisitor fileVisitor, ICollection<FileInfo> files)
+        {
+            fileVisitor.Visit(this, files);
+        }
+        public  long GetFileNumber(ICollection<FileInfo> files)
+        {
+            return files.Count();
+        }
+
+        public long GetFileSize(ICollection<FileInfo> files)
+        {
+            return files.Select(x => x.Length).Sum();
+        }
+    }
+
+    public class JPegFileUtility : IFileUtility
+    {
+        void Accept(IFileVisitor fileVisitor, ICollection<FileInfo> files)
+        {
+            fileVisitor.Visit(this,files);
+        }
+        public long GetFileNumber(ICollection<FileInfo> files)
+        {
+            return files.Where(x => x.Extension.ToLower() == "jpeg").Count();
+        }
+
+        public long GetFileSize(ICollection<FileInfo> files)
+        {
+            return  files.Where(x => x.Extension.ToLower() == "jpeg").Select(x => x.Length).Sum();
+        }
+    }
+
+    public class PngFileUtility : IFileUtility
+    {
+        void Accept(IFileVisitor fileVisitor, ICollection<FileInfo> files)
+        {
+            fileVisitor.Visit(this, files);
+        }
+        public long GetFileNumber(ICollection<FileInfo> files)
+        {
+            return files.Where(x => x.Extension.ToLower() == "png").Count();
+        }
+
+        public long GetFileSize(ICollection<FileInfo> files)
+        {
+            return files.Where(x => x.Extension.ToLower() == "png").Select(x => x.Length).Sum();
+        }
+    }
+
+    public interface IFileVisitor
+    {
+        long Visit(PngFileUtility fileUtility, ICollection<FileInfo> fileInfos);
+        long Visit(AllFileUtility fileUtility, ICollection<FileInfo> fileInfos);
+        long Visit(JPegFileUtility fileUtility, ICollection<FileInfo> fileInfos);
+    }
+    public class GetFileNumberFileVisitor : IFileVisitor 
+    {
+        public long Visit(PngFileUtility fileUtility, ICollection<FileInfo> fileInfos)
+        {
+            return fileUtility.GetFileNumber(fileInfos);
+        }
+
+        public long Visit(AllFileUtility fileUtility, ICollection<FileInfo> fileInfos)
+        {
+            return fileUtility.GetFileNumber(fileInfos);
+        }
+
+        public long Visit(JPegFileUtility fileUtility, ICollection<FileInfo> fileInfos)
+        {
+            return fileUtility.GetFileNumber(fileInfos);
+        }
+    }
+    public class GetFileSizeVisitor  : IFileVisitor 
+    {
+        public long Visit(PngFileUtility fileUtility, ICollection<FileInfo> fileInfos)
+        {
+            return fileUtility.GetFileSize(fileInfos);
+        }
+
+        public long Visit(AllFileUtility fileUtility, ICollection<FileInfo> fileInfos)
+        {
+            return fileUtility.GetFileSize(fileInfos);
+        }
+
+        public long Visit(JPegFileUtility fileUtility, ICollection<FileInfo> fileInfos)
+        {
+            return fileUtility.GetFileSize(fileInfos);
+        }
+    }
+}
+    
+
---
+++ visitor/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using visitor;
+using System.Collections.ObjectModel;
+
+var visitor = new GetFileNumberFileVisitor();
+var visitor2 = new GetFileSizeVisitor();
+
+var jPegFileUtility = new JPegFileUtility();
+var pngFileUtility = new PngFileUtility();
+var AllFileUtility = new AllFileUtility();
+
+var res =  visitor.Visit(jPegFileUtility, new Collection<FileInfo>());
+Console.WriteLine("All number of jpegfile = " + res);
+
+var res2  =visitor2.Visit(AllFileUtility, new Collection<FileInfo>()); 
+Console.WriteLine("Get size of all files = " + res2);
